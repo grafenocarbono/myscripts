fd0f3baf91898eb22f3c4acceb13b7325557b0dd
grafeno30 <109691187+grafenocarbono@users.noreply.github.com>
2024-03-11 20:24:13 +0100

    Create install_dotnet.sh

---
+++ install_dotnet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --version latest
+./dotnet-install.sh --version latest --runtime aspnetcore
+./dotnet-install.sh --channel 8.0
