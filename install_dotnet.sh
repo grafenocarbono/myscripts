8190791409b20b1e8f83b3408ea6c129d16cce92
grafeno30 <109691187+grafenocarbono@users.noreply.github.com>
2024-03-11 20:34:02 +0100

    Update install_dotnet.sh

--- install_dotnet.sh
+++ install_dotnet.sh
@@ -4,3 +4,7 @@ chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version latest
 ./dotnet-install.sh --version latest --runtime aspnetcore
 ./dotnet-install.sh --channel 8.0
+sudo snap install dotnet-sdk --classic
+wget https://aka.ms/dotnet-core-applaunch?framework=Microsoft.NETCore.App&framework_version=5.0.0&arch=x64&rid=linux-x64&os=ubuntu.22.04
+chmod +x dotnet-core-applaunch\?framework\=Microsoft.NETCore.App
+
