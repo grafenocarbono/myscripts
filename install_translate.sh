137825c24c5b85e174d1e4b2ff838cf743092dc9
grafenocarbono <grafeno30@gmail.com>
2024-04-04 11:51:49 +0200

    script install translate

--- install_translate.sh
+++ install_translate.sh
@@ -5,6 +5,6 @@ sudo mv ./trans /usr/local/bin
 sudo apt install gawk -y
 echo "Ejemplos de uso:\n" > traducir_instrucciones.txt
 echo "Modo interactivo: trans -shell -brief" >> traducir_instrucciones.txt
-echo "Traducir a español: trans :es "Hello Everyone" >> traducir_instrucciones.txt
-echo "Traducir a inglés: trans "Hola amiguitos" >> traducir_instrucciones.txt
-echo "trans -brief "Cómo estáis?" >> traducir_instrucciones.txt
+echo "Traducir a español: trans :es 'Hello Everyone'" >> traducir_instrucciones.txt
+echo "Traducir a inglés: trans 'Hola amiguitos'" >> traducir_instrucciones.txt
+echo "trans -brief 'Cómo estáis?'" >> traducir_instrucciones.txt
