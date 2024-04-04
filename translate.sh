6aa9b41ccf7d5a06644fec7c4675978b8d393412
grafeno30 <109691187+grafenocarbono@users.noreply.github.com>
2024-04-04 10:39:11 +0200

    Create translate.sh

---
+++ translate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget git.io/trans
+sudo chmod +x ./trans
+sudo mv ./trans /usr/local/bin
+sudo apt install gawk -y
+echo "Ejemplos de uso:\n" > traducir_instrucciones.txt
+echo "Modo interactivo: trans -shell -brief" >> traducir_instrucciones.txt
+echo "Traducir a español: trans :es "Hello Everyone" >> traducir_instrucciones.txt
+echo "Traducir a inglés: trans "Hola amiguitos" >> traducir_instrucciones.txt
+echo "trans -brief "Cómo estáis?" >> traducir_instrucciones.txt
