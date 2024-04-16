de081f7f60b11eb964bbe1de67b239fbaebd996d
grafeno30 <109691187+grafenocarbono@users.noreply.github.com>
2024-04-16 19:00:14 +0200

    Update install_metasploit.sh

--- install_metasploit.sh
+++ install_metasploit.sh
@@ -5,4 +5,4 @@ sudo apt install postgresql postgresql-contrib -y
 sudo systemctl enable --now postgresql
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod +x msfinstall
-echo "sudo ./msfinstall"
+sudo ./msfinstall
