e294876fdac34cb5aa90275b6af7a0f9f61c0080
grafeno30 <109691187+grafenocarbono@users.noreply.github.com>
2024-04-16 18:57:55 +0200

    Create install_metasploit.sh

---
+++ install_metasploit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install postgresql postgresql-contrib -y
+sudo systemctl enable --now postgresql
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+chmod +x msfinstall
+echo "sudo ./msfinstall"
