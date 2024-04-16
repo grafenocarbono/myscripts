bbdefb0929187bba1992a24754ab8bd0300fb428
grafeno30 <109691187+grafenocarbono@users.noreply.github.com>
2024-04-16 19:29:32 +0200

    Update install_metasploit.sh

--- install_metasploit.sh
+++ install_metasploit.sh
@@ -6,4 +6,7 @@ sudo systemctl enable --now postgresql
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod +x msfinstall
 sudo ./msfinstall
+msfdb init
+msfdb status
+read -n 1 -s -r -p "Press any key to continue"
 echo "how to boot: msfconsole"
