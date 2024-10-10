dae75fb86bce893a2996c8d131b4506bd5578958
grafenocarbono <109691187+grafenocarbono@users.noreply.github.com>
2024-10-10 16:24:17 +0200

    Update install_mongo_docker.sh

--- docker/mongo/install_mongo_docker.sh
+++ docker/mongo/install_mongo_docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-8.0.asc
+sudo apt-get install gnupg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
+mongosh --version
 docker run \
 -d -t \
 --name mongo_contenedor \
