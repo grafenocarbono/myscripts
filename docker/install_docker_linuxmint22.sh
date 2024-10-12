091616d8eee89d26dc96dd82d0c840766a041312
grafenocarbono <109691187+grafenocarbono@users.noreply.github.com>
2024-10-12 11:54:04 +0200

    Create install_docker_linuxmint22.sh

---
+++ docker/install_docker_linuxmint22.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu noble stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl is-active docker
+sudo usermod -aG docker ${USER}
+newgrp docker
+docker run hello-world
