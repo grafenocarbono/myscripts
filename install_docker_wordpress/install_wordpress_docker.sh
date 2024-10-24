c6b7a83c18260a35ea3fb321e26ab9cb860087e0
grafenocarbono <109691187+grafenocarbono@users.noreply.github.com>
2024-10-24 10:49:42 +0200

    Delete install_docker_wordpress directory

--- install_docker_wordpress/install_wordpress_docker.sh
+++
@@ -1,5 +0,0 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-docker-compose -f docker-compose.yml up
-firefox http://localhost:8080
