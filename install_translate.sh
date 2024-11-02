18c67ab7adf716f04ba6715530a706071695659f
grafenocarbono <109691187+grafenocarbono@users.noreply.github.com>
2024-11-02 09:32:21 +0100

    Update install_translate.sh

--- install_translate.sh
+++ install_translate.sh
@@ -3,8 +3,11 @@ wget git.io/trans
 sudo chmod +x ./trans
 sudo mv ./trans /usr/local/bin
 sudo apt install gawk -y
+sudo apt install snapd -y
+sudo snap install lngcnv
 echo "Ejemplos de uso:\n" > ~/traducir_instrucciones.txt
 echo "Modo interactivo: trans -shell -brief" >> ~/traducir_instrucciones.txt
 echo "Traducir a español: trans :es -brief 'Damn world'" >> ~/traducir_instrucciones.txt
 echo "Traducir a inglés: trans :en -brief 'Maldito mundo'" >> ~/traducir_instrucciones.txt
 echo "trans -brief 'Cómo estáis?'" >> ~/traducir_instrucciones.txt
+echo "lngcnv --ipa --eng "wears"" >> ~/traducir_instrucciones.txt
