158ce593c67afea14a57ed7af4e8cadbaec5e7a7
grafenocarbono <109691187+grafenocarbono@users.noreply.github.com>
2025-03-31 13:33:39 +0200

    Create zsh.sh

---
+++ zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install zsh
+zsh --version
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
