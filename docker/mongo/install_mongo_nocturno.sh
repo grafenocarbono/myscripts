5fc57766dfdd14a46d1a71e0b2de63638a16c45d
grafenocarbono <109691187+grafenocarbono@users.noreply.github.com>
2025-05-16 12:08:16 +0200

    Create install_mongo_nocturno.sh

---
+++ docker/mongo/install_mongo_nocturno.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-8.0.asc
+sudo apt-get install gnupg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
+mongosh --version
+docker run \
+-d -t \
+--name mongo_nocturno \
+-p 6666:27017 \
+-e MONGO_INITDB_ROOT_USERNAME=root \
+-e MONGO_INITDB_ROOT_PASSWORD=123qweASD_ \
+mongo
